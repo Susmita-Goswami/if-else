ceab54134a6b7a47fd8c2f99f2ecb1f4c1103f4d
Susmita-Goswami <108253713+Susmita-Goswami@users.noreply.github.com>
2022-08-26 11:16:45 +0600

    Add files via upload

---
+++ if else large.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+    int num1,num2;
+    printf("enter the first number =");
+    scanf("%d",&num1);
+
+    printf("enter the second number =");
+    scanf("%d",&num2);
+
+   if(num1>num2)
+   printf("Large=%d",num1);
+   else
+    printf("Large =%d",num2);
+
+
+
+
+return 0;
+}
---
+++ largenumber.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+    int time=10;
+    if(time==10)
+         {printf("Good morning\n");
+          printf("Susmita goswami");
+
+         }
+     else
+       {printf("sorry its not morning");
+        printf("Susmita goswami");
+
+       }
+
+
+
+    return 0;
+}
